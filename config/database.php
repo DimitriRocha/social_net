747e484b121ccbe1ef71c389fe92c9c55329414a
Dimitri Rocha <dimitrirocha10@gmail.com>
2018-11-22 09:21:09 -0300

    Alterações

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 // define("DB_USER", "root");
 // define("DB_PASS", "chuv@");
-	define("DB_USER", "dimi");
-	define("DB_PASS", "Darksiders1");
+	define("DB_USER", "social_net");
+	define("DB_PASS", "agbdlc1d@");
 	define("DB_SCHEMA", "social_net");
 	define("DB_HOST", "138.68.48.60");
